1b7c44cf823ca4e66316d5aa1bbca391130e78c6
Daniil Epikhov <depikhov29@gmail.com>
2022-10-10 12:36:49 +0300

    Initial commit

---
+++ src/main/java/org/example/school3bot/School3BotApplication.java
@@ -0,0 +1,13 @@
+package org.example.school3bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School3BotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(School3BotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/school3bot/School3BotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.school3bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class School3BotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
