5b2abb82b8647408ab75a23f92f7ac170b644f61
Daniil Epikhov <depikhov29@gmail.com>
2022-10-11 13:04:43 +0300

    SchoolBot class and pom dependency telegram added

---
+++ src/main/java/org/example/school3bot/telegram/SchoolBot.java
@@ -0,0 +1,34 @@
+package org.example.school3bot.telegram;
+
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.WebhookBot;
+
+public class SchoolBot extends TelegramWebhookBot {
+    private String botPath;
+    private String botUsername;
+    private String botToken;
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        return null;
+    }
+
+    @Override
+    public String getBotPath() {
+        return botPath;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
